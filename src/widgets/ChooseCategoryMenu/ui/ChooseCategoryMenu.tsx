31aa28a13e6e8fd67c398b227d68412e1fd1aa7b
Remi <noruto2025@ya.ru>
2023-09-12 21:23:45 +0300

    made choosing category menu

---
+++ src/widgets/ChooseCategoryMenu/ui/ChooseCategoryMenu.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import styled from "styled-components"
+import { Header } from "@widgets/ChooseCategoryMenu/ui/Header.tsx"
+// import { ChooseMenuLayout } from "@shared/ui/ChooseMenuLayout.tsx"
+
+export const ChooseCategory = () => {
+
+
+  return <ChooseMenuLayout>
+    <Header content={"EXPENSE"} />
+
+    qwe
+  </ChooseMenuLayout>
+}
+export const ChooseMenuLayout = styled.div`
+
+
+`
