2ae09f7dc38a336f66c0297d4b8a3318f1746fe4
Remi <noruto2025@ya.ru>
2023-10-03 22:00:32 +0300

    (server) making history

--- src/widgets/TransactionsMenu/ui/Transaction.tsx
+++ src/widgets/TransactionsMenu/ui/Transaction.tsx
@@ -132,7 +132,7 @@ const TransactionLayout = styled.div<{
   .quantity {
     align-self: flex-start;
     color: ${({ $type }) => $type === "expense" ? "var(--txt-8)" : "var(--txt-10)"};
-    font-family: Inter;
+    font-family: Inter, sans-serif;
     font-size: 15px;
     font-style: normal;
     font-weight: 400;
