81392a524e43bf379112fc4c8636824631184c6a
Remi <noruto2025@ya.ru>
2024-03-25 16:29:22 +0300

    adding manifest

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,48 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import path from "path"
 import svgr from "vite-plugin-svgr"
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa"
 
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Your Money",
+    short_name: "Your Money",
+    description: "No commerce app, that can help you to handle your money spending",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png"
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon"
+      },
+      {
+        src: "/maskable_icon.png",
+        sizes: "225x225",
+        type: "image/png",
+        purpose: "any maskable"
+      }
+    ],
+    theme_color: "white",
+    background_color: "#ececec",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait"
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +55,5 @@ export default defineConfig({
       "@widgets": path.resolve(__dirname, "./src/widgets")
     }
   },
-  plugins: [svgr(), react()] //https://github.com/pd4d10/vite-plugin-svgr?tab=readme-ov-file#usage
+  plugins: [svgr(), react(), VitePWA(manifestForPlugin)] //https://github.com/pd4d10/vite-plugin-svgr?tab=readme-ov-file#usage
 })
